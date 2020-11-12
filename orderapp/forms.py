ac46e5c49cd8e66ace05910a808e2a8de7c911b7
Aleksey Ostryakov <ostrav89@yandex.ru>
2020-11-13 01:45:53 +0500

    added select_related

--- orderapp/forms.py
+++ orderapp/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 
+from mainapp.models import Product
 from orderapp.models import Order, OrderItem
 
 
@@ -24,6 +25,6 @@ class OrderItemForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super(OrderItemForm, self).__init__(*args, **kwargs)
-
+        self.fields['product'].queryset = Product.get_items().select_related()
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'form-control'
