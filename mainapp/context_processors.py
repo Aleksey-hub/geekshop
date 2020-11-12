d0be64b6a8559d34d98f0d58901d3a41c89fa00c
Aleksey Ostryakov <ostrav89@yandex.ru>
2020-11-13 01:18:26 +0500

    added select_related

--- mainapp/context_processors.py
+++ mainapp/context_processors.py
@@ -4,8 +4,7 @@ from basketapp.models import Basket
 def basket(request):
     basket_items = []
     if request.user.is_authenticated:
-        basket_items = Basket.objects.filter(user=request.user).select_related()
-        # basket_items = request.user.basket.select_related()
+        basket_items = Basket.objects.filter(user=request.user)
 
     return {
         'basket': basket_items
