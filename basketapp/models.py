7c11a810c89ec1bf94fe1483d002e4bc861ac625
Aleksey Ostryakov <ostrav89@yandex.ru>
2020-11-14 15:49:05 +0500

    added @cached_property     def get_items_cached(self):

--- basketapp/models.py
+++ basketapp/models.py
@@ -20,7 +20,7 @@ class Basket(models.Model):
     # objects = BasketQuerySet.as_manager()
 
     # user = models.ForeignKey(ShopUser, on_delete=models.CASCADE)
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='basket')
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=0, verbose_name='количество')
     add_datetime = models.DateTimeField(auto_now_add=True, verbose_name='время добавления')
