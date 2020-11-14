5d6b735fb520dadafa0b31f6ae11e1ab915a2187
Aleksey Ostryakov <ostrav89@yandex.ru>
2020-11-14 14:45:24 +0500

    edit orderapp/models.py

--- orderapp/views.py
+++ orderapp/views.py
@@ -80,15 +80,14 @@ class OrderItemsUpdate(UpdateView):
 
     def get_context_data(self, **kwargs):
         data = super(OrderItemsUpdate, self).get_context_data(**kwargs)
+        data['product'] = OrderItem.get_items().select_related()
 
         OrderFormSet = inlineformset_factory(Order, OrderItem, form=OrderItemForm, extra=1)
         if self.request.POST:
-            # data['orderitems'] = OrderFormSet(self.request.POST, instance=self.object)
-            data['orderitems'] = OrderFormSet(self.request.POST, instance=self.object.select_related())
+            data['orderitems'] = OrderFormSet(self.request.POST, instance=self.object)
         else:
             queryset = self.object.orderitems.select_related()
             formset = OrderFormSet(instance=self.object, queryset=queryset)
-            # formset = OrderFormSet(instance=self.object.select_related(), queryset=queryset)
             for form in formset.forms:
                 if form.instance.pk:
                     form.initial['price'] = form.instance.product.price
