6d648cc576a8a020ebbd466a18db930ca9945e66
Aleksey Ostryakov <ostrav89@yandex.ru>
2020-11-15 20:21:41 +0500

    ...

--- orderapp/views.py
+++ orderapp/views.py
@@ -145,9 +145,9 @@ def product_quantity_update_save(sender, update_fields, instance, **kwargs):
         if instance.pk:
             instance.product.quantity = F('quantity') - (instance.quantity - sender.get_item(instance.pk).quantity)
         else:
-            # instance.product.quantity -= instance.quantity
+            instance.product.quantity -= instance.quantity
             # instance.product.quantity = F('quantity') - instance.quantity
-            instance.product.quantity = F('quantity') - 1
+            # instance.product.quantity = F('quantity') - 1
         instance.product.save()
 
 
