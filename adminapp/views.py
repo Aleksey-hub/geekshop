9598ae374407545712480226ae43703b164bc72f
Aleksey Ostryakov <ostrav89@yandex.ru>
2020-11-15 16:05:44 +0500

    added product_is_active_update_productcategory_save in adminapp/views.py

--- adminapp/views.py
+++ adminapp/views.py
@@ -1,5 +1,8 @@
 from django.contrib.auth.decorators import user_passes_test
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+from django.db import connection
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse, reverse_lazy
@@ -105,8 +108,8 @@ class UserDeleteView(DeleteView):
     def delete(self, request, *args, **kwargs):
         self.object = self.get_object()
         if self.object.is_active == True:
-            #self.object.is_active = False
-            #self.object.save()
+            # self.object.is_active = False
+            # self.object.save()
             self.object.delete()
         else:
             self.object.is_active = True
@@ -338,3 +341,19 @@ def product_delete(request, pk):
         'product_to_delete': product
     }
     return render(request, 'adminapp/product_delete.html', content)
+
+
+def db_profile_by_type(prefix, type, queries):
+    update_queries = list(filter(lambda x: type in x['sql'], queries))
+    print(f'db_profile {type} for {prefix} :')
+    [print(query['sql']) for query in update_queries]
+
+
+@receiver(pre_save, sender=ProductCategory)
+def product_is_active_update_productcategory_save(sender, instance, **kwargs):
+    if instance.pk:
+        if instance.is_active:
+            instance.product_set.update(is_active=True)
+        else:
+            instance.product_set.update(is_active=False)
+        db_profile_by_type(sender, 'UPDATE', connection.queries)
