f9e946104243f08a40f675659120aa9a1a9d41f0
Aleksey Ostryakov <ostrav89@yandex.ru>
2020-11-18 00:11:28 +0500

    edit authapp/tests.py

--- authapp/tests.py
+++ authapp/tests.py
@@ -87,8 +87,8 @@ class TestUserManagement(TestCase):
 
         new_user = ShopUser.objects.get(username=new_user_data['username'])
 
-        activation_url = f"{settings.DOMAIN_NAME}/auth/verify/{new_user_data['email']}/{new_user.activation_key}/"
-        assert False, activation_url
+        activation_url = f"{settings.DOMAIN_NAME}/auth/verify/{new_user_data['email']}/{new_user.activation_key}"
+        # assert False, activation_url
         response = self.client.get(activation_url)
         self.assertEqual(response.status_code, 200)
 
