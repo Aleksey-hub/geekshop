46774ce857b4edcf066315925d4cb164cd366b66
Aleksey Ostryakov <ostrav89@yandex.ru>
2020-11-17 23:49:34 +0500

    edit authapp/tests.py

--- authapp/views.py
+++ authapp/views.py
@@ -25,7 +25,8 @@ def verify(request, email, activation_key):
         if user.activation_key == activation_key and not user.if_activation_key_expired():
             user.is_active = True
             user.save()
-            auth.login(request, user)
+            # auth.login(request, user)
+            auth.login(request)
             return render(request, 'authapp/verification.html')
         print(f'error activation user: {email}')
         return render(request, 'authapp/verification.html')
