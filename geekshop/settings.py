7612d3109b9a68d6cfe130672f7e510e13464975
Aleksey Ostryakov <ostrav89@yandex.ru>
2020-11-17 22:04:40 +0500

    edit authapp/tests.py

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -153,7 +153,8 @@ LOGIN_URL = '/auth/login/'
 
 LOGIN_ERROR_URL = '/'
 
-DOMAIN_NAME = 'http://localhost:8000'
+# DOMAIN_NAME = 'http://localhost:8000'
+DOMAIN_NAME = 'http://5.63.157.18'
 EMAIL_HOST = 'localhost'
 EMAIL_PORT = '25'
 EMAIL_HOST_USER = 'django@geekshop.local'
